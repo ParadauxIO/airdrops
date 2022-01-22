30615092c942c31e860bf72e8912d8008ee10a78
Rían Errity <rian@paradaux.io>
2022-01-22 17:48:42 +0000

    Initial setup

--- src/main/java/io/paradaux/airdrops/tasks/DropTask.java
+++ src/main/java/io/paradaux/airdrops/tasks/DropTask.java
@@ -1,2 +1,8 @@
-package io.paradaux.airdrops.tasks;public class DropTask {
+package io.paradaux.airdrops.tasks;
+
+public class DropTask implements Runnable {
+    @Override
+    public void run() {
+        // Get random
+    }
 }
